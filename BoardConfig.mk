da729509fa9592bdbf1097c89116053c50eaf0f7
PabloG <gpablo1@yahoo.com.ar>
2026-07-09 21:02:53 +0200

    Add ANT wireless device configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):init_xiaomi_umi
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/umi.config
 
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := umi
 
